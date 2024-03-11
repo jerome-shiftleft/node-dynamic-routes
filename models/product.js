277fa75120c063cfb74acbacf8f1950633b2f835
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-12 04:43:31 +0800

    Refactor code and add findById method

--- models/product.js
+++ models/product.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
+  "data",
+  "products.json"
 );
 
-const getProductsFromFile = cb => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -19,8 +19,8 @@ const getProductsFromFile = cb => {
 
 module.exports = class Product {
   constructor(title, imageUrl, description, price) {
-    this.id = Date.now();
-    console.log(`id: ${this.id}`)
+    this.id = Date.now().toString();
+    //console.log(`id: ${this.id}`)
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -29,9 +29,10 @@ module.exports = class Product {
 
   save() {
     //this.id = Math.random().toString();
-    getProductsFromFile(products => {
+    //this.id = Date.now().toString();
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+      fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
@@ -39,5 +40,12 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
   }
 };
