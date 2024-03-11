bd0e9c56fb0f27dd46a7fe987d06b38e7729788a
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-12 05:18:14 +0800

    Add product detail page and CSS

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,9 +14,14 @@ exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   //console.log(`prodId: ${prodId}`);
   Product.findById(prodId, (product) => {
-    console.log(product);
+    //console.log(product);
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: `/products/${product.id}`
+    })
   })
-  res.redirect("/");
+  //res.redirect("/");
 };
 
 exports.getIndex = (req, res, next) => {
