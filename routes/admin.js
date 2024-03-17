26f8da085787bbc1f36ce9b17a8d641f6ec3eda9
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-17 20:51:43 +0800

    progress

--- routes/admin.js
+++ routes/admin.js
@@ -8,4 +8,5 @@ router.get("/add-product", adminController.getAddProduct);
 router.get("/products", adminController.getProducts);
 // /admin/add-product => POST
 router.post("/add-product", adminController.postAddProduct);
+router.get("/edit-product/:productId", adminController.getEditProduct);
 module.exports = router;
