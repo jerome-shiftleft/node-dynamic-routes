9f1f5cdfccd558fa25b1123c1486ebe5a7e1d43c
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-17 23:31:45 +0800

    progress

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,7 +11,7 @@ exports.getAddProduct = (req, res, next) => {
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
-    res.redirect("/");
+    return res.redirect("/");
   }
 
   const prodId = req.params.productId;
@@ -31,16 +31,29 @@ exports.getEditProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
+  const price = parseFloat(req.body.price);
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect("/");
 };
 
 exports.postEditProduct = (req, res, next) => {
-  
-}
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = parseFloat(req.body.price);
+  const udpatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    udpatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products')
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
