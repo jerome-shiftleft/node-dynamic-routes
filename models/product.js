e2ac7a83d493880b69d73c1bf5f9a03d435f5993
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-19 23:41:00 +0800

    progress

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const Cart = require('./cart');
 
 const p = path.join(
   path.dirname(require.main.filename),
@@ -49,9 +50,12 @@ module.exports = class Product {
 
   static deleteById(id) {
     getProductsFromFile((products) => {
+      const product = products.find(products => prod.id === prod.id ===id)
       const updatedProducts = products.filter((prod) => prod.id !== id);
       fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-        console.log(err)
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
       });
     });
   }
